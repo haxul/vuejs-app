aa01d4689dc06081f3b71e57441ffdc7a1c64db8
haxul <s.s.starodubov@yandex.ru>
2020-10-07 20:09:29 +0400

    init

---
+++ app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Input :text="'Hello Input'"/>
+    </div>
+</template>
+
+<script>
+    import Input from "./components/Input";
+
+    export default {
+        name : "App",
+        components: {Input}
+    }
+
+</script>
