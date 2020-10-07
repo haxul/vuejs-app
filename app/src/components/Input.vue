45dd172ef797e093e9cf57e5df8ab5c93ca4a4b8
haxul <s.s.starodubov@yandex.ru>
2020-10-07 20:05:36 +0400

    init

---
+++ app/src/components/Input.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h4>This is {{text}}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Input",
+        props: {
+            text: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
